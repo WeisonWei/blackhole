b2b33bb430648e255159a7564de4ae7f79df999d
lihanran <lihanran@jd.com>
2021-10-30 21:15:56 +0800

    BH-4 新增多线程消费多topic的功能

--- src/main/java/com/bh/config/Bootstrap.java
+++ src/main/java/com/bh/config/Bootstrap.java
@@ -1,36 +1,63 @@
 package com.bh.config;
 
-import com.bh.message.kafka.KfkConsumer;
-import com.bh.util.ClassUtil;
-import com.bh.util.SpringUtil;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+import com.bh.domain.Topics;
+import com.bh.message.kafka.KfkConsumer;
+import com.bh.util.JsonUtil;
+import com.bh.util.SpringUtil;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ObjectUtils;
 
 //@ConditionalOnClass(ClassUtil.class)
 @Configuration
 public class Bootstrap {
-
+  private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
   public static final Map<String, Thread> bootstrapThreads = new HashMap<>();
 
   @Resource
   SpringUtil springUtil;
 
-  @Resource
-  private KfkConsumer kfkConsumer;
+  @Autowired
+  KfkConf kfkConf;
 
   @PostConstruct
   public void setUp() {
-    Object consumer = SpringUtil.getBean("KfkConsumer");
-    if (consumer instanceof KfkConsumer) {
-      Thread kfkConsumer = new Thread((KfkConsumer) consumer);
-      bootstrapThreads.put("kfkConsumer", kfkConsumer);
-      kfkConsumer.start();
+    JsonArray jsonArray = JsonParser.parseString(kfkConf.getTopics()).getAsJsonArray();
+    if (!ObjectUtils.isEmpty(jsonArray)) {
+      for (JsonElement json : jsonArray) {
+        Object consumer = SpringUtil.getBean("KfkConsumer");
+        if (consumer instanceof KfkConsumer) {
+          Topics kfkSon = JsonUtil.str2Bean(json.toString(), Topics.class);
+          Boolean b = checkListKfk(kfkSon);
+          if (!b) {
+            logger.warn("kfk list :" + kfkSon.toString());
+            continue;
+          }
+          ((KfkConsumer) consumer).changeConsumer(kfkSon.getUserName(), kfkSon.getPassWord(), kfkSon.getBrokerList(),
+              kfkSon.getGroupId(), kfkSon.getTopic());
+          Thread kfkConsumer = new Thread((KfkConsumer) consumer);
+          bootstrapThreads.put(kfkSon.getType(), kfkConsumer);
+          kfkConsumer.start();
+        }
+      }
     }
+  }
+
+  public Boolean checkListKfk(Topics kfkSon) {
+    if (ObjectUtils.isEmpty(kfkSon.getUserName()) || ObjectUtils.isEmpty(kfkSon.getBrokerList()) ||
+        ObjectUtils.isEmpty(kfkSon.getGroupId()) || ObjectUtils.isEmpty(kfkSon.getType()) || ObjectUtils.isEmpty(kfkSon.getTopic())) {
+      return false;
+    }
+    return true;
   }
 }
---
+++ src/main/java/com/bh/domain/Topics.java
@@ -0,0 +1,58 @@
+package com.bh.domain;
+
+public class Topics {
+  private String type;
+  private String userName;
+  private String passWord;
+  private String brokerList;
+  private String groupId;
+  private String topic;
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getPassWord() {
+    return passWord;
+  }
+
+  public void setPassWord(String passWord) {
+    this.passWord = passWord;
+  }
+
+  public String getBrokerList() {
+    return brokerList;
+  }
+
+  public void setBrokerList(String brokerList) {
+    this.brokerList = brokerList;
+  }
+
+  public String getGroupId() {
+    return groupId;
+  }
+
+  public void setGroupId(String groupId) {
+    this.groupId = groupId;
+  }
+
+  public String getTopic() {
+    return topic;
+  }
+
+  public void setTopic(String topic) {
+    this.topic = topic;
+  }
+}

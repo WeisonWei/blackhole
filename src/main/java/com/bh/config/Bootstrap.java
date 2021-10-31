af410d75e0bd77ae4943db2a8bf01d6fd6a3f8c9
lihanran <lihanran@jd.com>
2021-10-31 23:14:14 +0800

    BH-4 新增多线程消费多topic的功能

--- src/main/java/com/bh/config/Bootstrap.java
+++ src/main/java/com/bh/config/Bootstrap.java
@@ -11,6 +11,8 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ObjectUtils;
@@ -18,7 +20,7 @@ import org.springframework.util.ObjectUtils;
 //@ConditionalOnClass(ClassUtil.class)
 @Configuration
 public class Bootstrap {
-
+  private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
   public static final Map<String, Thread> bootstrapThreads = new HashMap<>();
 
   @Resource
@@ -35,6 +37,11 @@ public class Bootstrap {
         Object consumer = SpringUtil.getBean("KfkConsumer");
         if (consumer instanceof KfkConsumer) {
           JsonObject kfkSon = json.getAsJsonObject();
+          Boolean b = checkListKfk(kfkSon);
+          if (!b) {
+            logger.warn("kfk list :" + kfkSon.getAsString());
+            continue;
+          }
           ((KfkConsumer) consumer).changeConsumer(kfkSon.get("username").getAsString(), kfkSon.get("password").getAsString(), kfkSon.get("brokerList").getAsString(), kfkSon.get("groupId").getAsString(), kfkSon.get("topic").getAsString());
           Thread kfkConsumer = new Thread((KfkConsumer) consumer);
           bootstrapThreads.put(kfkSon.get("type").getAsString(), kfkConsumer);
@@ -42,5 +49,12 @@ public class Bootstrap {
         }
       }
     }
+  }
+
+  public Boolean checkListKfk(JsonObject kfkSon) {
+    if (ObjectUtils.isEmpty(kfkSon.get("username")) || ObjectUtils.isEmpty(kfkSon.get("password")) || ObjectUtils.isEmpty(kfkSon.get("brokerList")) || ObjectUtils.isEmpty(kfkSon.get("groupId")) || ObjectUtils.isEmpty(kfkSon.get("topic"))) {
+      return false;
+    }
+    return true;
   }
 }
